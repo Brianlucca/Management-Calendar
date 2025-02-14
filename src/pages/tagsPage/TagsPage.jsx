211be59bc888373a0aa318d19daea2b2bd4c3eaf
Brianlucca <brianlucca2002@gmail.com>
2025-02-14 15:47:34 -0300

    feat: tag screen

---
+++ src/pages/tagsPage/TagsPage.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import TagsManager from "../../components/tags/TagsManager";
+import TagsLegend from "../../components/tags/tagsLegend/TagsLegend";
+
+const TagsPage = () => {
+  const [tags, setTags] = useState([]);
+
+  const handleTagCreate = (newTag) => {
+    setTags([...tags, newTag]);
+  };
+
+  const handleTagDelete = (deletedTags) => {
+    setTags(tags.filter(tag => !deletedTags.includes(tag.id)));
+  };
+
+  return (
+    <div className="md:mt-0 mt-16 min-h-screen p-6 bg-gray-50 flex flex-col items-center">
+      <div className="w-full max-w-5xl space-y-6">
+        <TagsManager onTagCreate={handleTagCreate} />
+        <TagsLegend tags={tags} onTagDelete={handleTagDelete} />
+      </div>
+    </div>
+  );
+};
+
+export default TagsPage;
