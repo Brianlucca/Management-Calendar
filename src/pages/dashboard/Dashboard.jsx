a5c6acf36c80f867d8777fb7b7b9914020b1936e
Brianlucca <brianlucca2002@gmail.com>
2025-02-14 15:47:04 -0300

    feat: dashboard screen

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,14 +3,13 @@ import Calendar from "../../components/calendar/Calendar";
 
 const Dashboard = () => {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <header className="bg-white shadow p-4">
-        <h1 className="text-2xl font-bold text-center">Management Calendar</h1>
-      </header>
-      <main className="p-4">
-        <Calendar />
-      </main>
-    </div>
+    <main>
+      <div>
+        <div>
+          <Calendar />
+        </div>
+      </div>
+    </main>
   );
 };
 
