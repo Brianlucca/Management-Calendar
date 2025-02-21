b139c3e2bedb916d0624370b282d6ef8242a55a8
Brianlucca <brianlucca2002@gmail.com>
2025-02-20 22:54:49 -0300

    refactored menu and sidebar

--- src/components/menu/mobileMenu/MobileMenu.jsx
+++ src/components/menu/mobileMenu/MobileMenu.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { LayoutDashboard, ShieldCheck, User, LogOut, Users, ChevronDown, Menu, Tags } from "lucide-react";
-import { useAuth } from "../../../context/AuthContext";
+import { LayoutDashboard, User, LogOut, Menu, Tags } from "lucide-react";
 import { logout } from "../../../service/AuthService/AuthService";
+import { useNotification } from "../../../components/notification/Notification";
 
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [adminOpen, setAdminOpen] = useState(false);
-  const { userRole } = useAuth();
   const location = useLocation();
+  const { showNotification } = useNotification();
+  
 
   const handleLogout = async () => {
     try {
       await logout();
     } catch (error) {
-      console.error("Logout failed", error);
+      showNotification(`Logout failed`, "error");
     }
   };
 
@@ -24,7 +24,7 @@ const MobileMenu = () => {
         return "Dashboard";
       case "/admin":
         return "Administração";
-      case "/perfil":
+      case "/profile":
         return "Perfil";
       case "/tags":
         return "Gerenciar Tags";
@@ -54,27 +54,8 @@ const MobileMenu = () => {
                 <LayoutDashboard /> <span>Dashboard</span>
               </Link>
             </li>
-            {userRole === "administrador" && (
-              <li>
-                <button onClick={() => setAdminOpen(!adminOpen)} className="flex items-center justify-between w-full p-2 hover:bg-gray-800 rounded">
-                  <div className="flex items-center space-x-2">
-                    <ShieldCheck /> <span>Administrador</span>
-                  </div>
-                  <ChevronDown className={`${adminOpen ? "rotate-180" : ""} transition-transform`} />
-                </button>
-                {adminOpen && (
-                  <ul className="ml-6 space-y-2">
-                    <li>
-                      <Link to="/admin" className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
-                        <Users /> <span>Gerenciador de Usuários</span>
-                      </Link>
-                    </li>
-                  </ul>
-                )}
-              </li>
-            )}
             <li>
-              <Link to="/perfil" className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
+              <Link to="/profile" className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
                 <User /> <span>Perfil</span>
               </Link>
             </li>
--- src/components/menu/sideBar/SideBar.jsx
+++ src/components/menu/sideBar/SideBar.jsx
@@ -1,72 +1,57 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { LayoutDashboard, ShieldCheck, User, LogOut, Users, ChevronDown, Menu, Tags } from "lucide-react";
-import { useAuth } from "../../../context/AuthContext";
+import { LayoutDashboard, User, LogOut, Menu, Tags } from "lucide-react";
 import { logout } from "../../../service/AuthService/AuthService";
+import { useNotification } from "../../notification/Notification";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [adminOpen, setAdminOpen] = useState(false);
-  const { userRole } = useAuth();
+  const { showNotification } = useNotification();
 
   const handleLogout = async () => {
     try {
       await logout();
     } catch (error) {
-      console.error("Logout failed", error);
+       showNotification(`Logout failed`, "error");
     }
   };
 
   return (
     <main className={`z-50 flex fixed top-0 left-0 h-full ${isOpen ? "w-64" : "w-20"} bg-gray-900 text-white p-4 transition-all duration-300`}>
       <nav className="w-full h-full flex flex-col">
-        <button 
-          onClick={() => setIsOpen(!isOpen)} 
-          className="text-xl mb-4 self-start p-2 rounded hover:bg-gray-800"
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="text-xl mb-4 self-start p-3 rounded hover:bg-gray-800"
         >
           <Menu />
         </button>
         <ul className="space-y-4 flex-1">
           <li>
-            <Link to="/" className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
+            <Link to="/" className="text-white flex items-center space-x-2 p-3 hover:bg-gray-800 rounded">
               <LayoutDashboard /> <span className={isOpen ? "block" : "hidden"}>Dashboard</span>
             </Link>
           </li>
-          {userRole === "administrador" && (
-            <li>
-              <button onClick={() => setAdminOpen(!adminOpen)} className="flex items-center justify-between w-full p-2 hover:bg-gray-800 rounded">
-                <div className="flex items-center space-x-2">
-                  <ShieldCheck /> <span className={isOpen ? "block" : "hidden"}>Administrador</span>
-                </div>
-                {isOpen && <ChevronDown className={`${adminOpen ? "rotate-180" : ""} transition-transform`} />}
-              </button>
-              {adminOpen && (
-                <ul className={`ml-6 space-y-2 ${isOpen ? "block" : "hidden"}`}>
-                  <li>
-                    <Link to="/admin" className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
-                      <Users /> <span>Gerenciador de Usuários</span>
-                    </Link>
-                  </li>
-                </ul>
-              )}
-            </li>
-          )}
           <li>
-            <Link to="/perfil" className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
+            <Link to="/profile" className="text-white flex items-center space-x-2 p-3 hover:bg-gray-800 rounded">
               <User /> <span className={isOpen ? "block" : "hidden"}>Perfil</span>
             </Link>
           </li>
           <li>
-            <Link to="/tags" className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
+            <Link to="/tags" className="text-white flex items-center space-x-2 p-3 hover:bg-gray-800 rounded">
               <Tags /> <span className={isOpen ? "block" : "hidden"}>Gerenciar Tags</span>
             </Link>
           </li>
           <li>
-            <button onClick={handleLogout} className="text-white flex items-center space-x-2 p-2 hover:bg-gray-800 rounded">
+            <button onClick={handleLogout} className="text-white flex items-center space-x-2 p-3 hover:bg-gray-800 rounded">
               <LogOut /> <span className={isOpen ? "block" : "hidden"}>Logout</span>
             </button>
           </li>
         </ul>
+        <hr className="my-4 border-gray-700" />
+        <div className={`text-center text-gray-500 text-sm ${isOpen ? "" : "whitespace-normal break-words"}`}>
+          <p className={isOpen ? "" : "text-xs"}>@ 2025 Management Calendar</p>
+          <p className={isOpen ? "" : "text-xs"}>Versão 1.0.0</p>
+        </div>
       </nav>
     </main>
   );
