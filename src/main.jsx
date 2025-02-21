2d8b1d001849a0c8ab7c95fe2b935ed16faf318e
Brianlucca <brianlucca2002@gmail.com>
2025-02-20 22:46:47 -0300

    added global imports to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/index.css'
+import './css/scrollBar.css'
+import './css/headerToolbarCalendar.css'
+import './css/showNotification.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
